43860f482032686491565f5514d33359736a97f9
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-04-16 18:42:14 -0500

    full contact section, nothing linked

--- assets/javascript/updated.js
+++ assets/javascript/updated.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+
+    
+});
+
+
+
+
+
+//   $(function () {
+//     $(document).scroll(function () {
+//       var $nav = $(".navbar-color");
+//       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//     });
+//   });
+
+$(document).ready(function(){
+    $(window).scroll(function() { // check if scroll event happened
+      if ($(document).scrollTop() > 800) { // check if user scrolled more than 50 from top of the browser window
+        $(".navbar-color").css("background-color", "#dadce0"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+      } else {
+        $(".navbar-color").css("background-color", "transparent"); // if not, change it back to transparent
+      }
+    });
   });
-
-
-  
