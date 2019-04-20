1c7c563f033a52579d4bedffa71950b5c37909cc
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-04-20 15:04:00 -0500

    some responsiveness, but not all the way there. The button, logo, and navbar color are not

--- assets/javascript/updated.js
+++ assets/javascript/updated.js
@@ -16,11 +16,11 @@ $(document).ready(function(){
 //   });
 
 $(document).ready(function(){
-    $(window).scroll(function() { // check if scroll event happened
-      if ($(document).scrollTop() > 800) { // check if user scrolled more than 50 from top of the browser window
-        $(".navbar-color").css("background-color", "#dadce0"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+    $(window).scroll(function() { 
+      if ($(document).scrollTop() > 800) { 
+        $(".navbar-color").css("background-color", "#dadce0"); 
       } else {
-        $(".navbar-color").css("background-color", "transparent"); // if not, change it back to transparent
+        $(".navbar-color").css("background-color", "transparent"); 
       }
     });
   });
